404b1b82e13661640fb46aea7f75a89a518e2ff5
Rai-Aaditya <adityaanchalarai@gmail.com>
2023-04-10 21:47:37 +0530

    Todo app stage:1

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const date = require(__dirname + "/date.js");
+const app = express();
+
+
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    let day = date();
+    res.render('list', {listTitle: day, newListItems: items});
+});
+
+app.post("/", (req, res)=>{
+    let item = req.body.newItem;
+    if(item.length>0 && item!=" "){
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    }
+})
+
+app.get("/work", (req, res)=>{
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", (req, res)=>{
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.listen(3000, ()=>{
+    console.log("Server Started on port 3000");
+});
